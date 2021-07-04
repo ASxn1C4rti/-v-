b1ebfe77b4422284e91e78846c0fcd281beb0a81
Jose E. Hernandez <78750060+xASonicMenx@users.noreply.github.com>
2021-07-03 19:11:13 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,5 @@
+class Main{  
+  public static void main(String args[]){  
+    System.out.println("Hola, Mi Nombre es Jose Emilio Juárez Hernández, tengo 16 años, soy programador joven de informatica de desarrollacion de Java, Python & Node.js, actualmente soy Trabajo para @TobitTheBot como Desarrollador de ChatBot y administrador del servidor y para Lifeboat Network (lbsg.net) como Traductor de Español.");  
+  }  
+}  

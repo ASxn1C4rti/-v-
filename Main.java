068dbf31dc99b7a28fd714818125db463881ae85
Jose E. Hernandez <78750060+xASonicMenx@users.noreply.github.com>
2021-07-03 19:13:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main{  
   public static void main(String args[]){  
-    System.out.println("Hola, Mi Nombre es Jose Emilio Juárez Hernández, tengo 16 años, soy programador joven de informatica de desarrollacion de Java, Python & Node.js, actualmente soy Trabajo para @TobitTheBot como Desarrollador de ChatBot y administrador del servidor y para Lifeboat Network (lbsg.net) como Traductor de Español.");  
+    System.out.println("Hola, Mi Nombre es Jose Emilio Juárez Hernández, tengo 16 años, soy un desarrollador joven de Java, Python & Node.js, actualmente soy Trabajo para @TobitTheBot como Desarrollador de ChatBot y administrador del servidor y para Lifeboat Network (lbsg.net) como Traductor de Español.");  
   }  
 }  
